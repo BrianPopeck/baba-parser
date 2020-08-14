6072a85121c1a961376ef471afa12fcb2de49072
BrianPopeck <bpopeck4@gmail.com>
2020-08-14 00:03:24 -0400

    fixed comment

--- terminal_symbols.py
+++ terminal_symbols.py
@@ -1,4 +1,4 @@
-TERMINAL_SYMBOLS = {    # from Fandom wiki; no support for Letters
+TERMINAL_SYMBOLS = {    # from Fandom wiki; does not support for Letters
     'T_And': ['AND'],
     'T_Not': ['NOT'],
     'T_Noun': ['ALGAE', 'ALL', 'ANNI', 'BABA', 'BAT', 'BELT', 'BIRD', 'BOG', 'BOLT', 'BOX', 'BRICK', 'BUBBLE', 'BUG', 'CAKE', 'CLIFF', 'CLOUD', 'COG', 'CRAB', 'CURSOR', 'DOOR', 'DUST', 'EMPTY', 'FENCE', 'FIRE', 'FLAG', 'FLOWER', 'FOLIAGE', 'FRUIT', 'FUNGUS', 'GHOST', 'GRASS', 'GROUP', 'HAND', 'HEDGE', 'ICE', 'IMAGE', 'JELLY', 'KEKE', 'KEY', 'LAVA', 'LEAF', 'LEVEL', 'LINE', 'LOVE', 'ME', 'MOON', 'ORB', 'PILLAR', 'PIPE', 'ROBOT', 'ROCK', 'ROCKET', 'ROSE', 'RUBBLE', 'SKULL', 'STAR', 'STATUE', 'SUN', 'TEXT', 'TILE', 'TREE', 'UFO', 'WALL', 'WATER'],
